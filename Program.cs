c64785a977064a2917eb611a87ba6b0adcfef9be
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-24 15:05:54 +0530

    [Madhur] - Length of line Added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparisonUsingOOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateLengthOfLine calculate = new CalculateLengthOfLine();
+            calculate.LengthOfLine();
+        }
+    }
+}
